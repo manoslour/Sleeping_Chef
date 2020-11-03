4046d4aa6c2e884d0cda907134dc73ad817220e7
Manos Lourakis <lourakismanos@gmail.com>
2020-11-03 17:51:01 +0200

    Added slacking chef print

--- takeaway.c
+++ takeaway.c
@@ -12,18 +12,20 @@ int main(){
 
     pthread_create(&p_chef, NULL, chef, NULL);
 
+    printf("\n");
     for(t = 0; t < MAX_CUSTOMERS; t++) {
-        //printf("In main: creating thread %ld\n", (long) t);
         rc = pthread_create(&p_customers[t], NULL, customer, (void *)t);
         if(rc) {
             perror("Failed to create thread\n");
         }
         if( (t+1)%3 == 0) {
-            sleep(3);
-            printf("\n\n");
+            sleep(1);
+            printf("\n*** Chef is on facebook ***\n");
+            printf("\n");
+            sleep(2);
         }
     }
 
-    sleep(4);
+    sleep(3);
     printf("\nMain thread exiting\n");
 }
